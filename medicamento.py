e38d4376818592d8b599603866f8985d15f415c2
Arthur Silva Vilela <arthur.silva.vilela@gmail.com>
2026-09-17 18:29:45 -0300

    agora criei a funçao lista  medicamentos ao meu codigo e agora so falta mais uma funçao para meu codigo

--- medicamento.py
+++ medicamento.py
@@ -3,21 +3,45 @@ def cadrastro_remedios():
 
  print("\ncadrastro de remedios")
      
-codigo = input("digite o codigo do remedio: ")
 nome = input("digite o nome do remedio: ")
+codigo = input("digite o codigo do remedio: ")
 forma_farmaceutica = input("digite a forma farmaceutica: ")
 preço = input("digite o preço")
     
 novo_remedio = {
         "codigo": codigo,
         "nome": nome,
-        "forma_farmaceutica": forma_farmaceutica,
+        "form_farmaceutica": forma_farmaceutica,
         "preço": preço,
     }
-remedios.append(novo_remedio)
+remedio.append(novo_remedio)
 
 print("novo remedio cadastrado")
 
+
+
+def lista_medicamentos():
+    print("lista de livros")
+if len == 0:
+    print("nem um remedio no estoque")
+    
+    
+    termo_busca = input("digite o codigo do remedio: ")
+    encontrado = False
+    
+    for remedios in remedio:
+        if termo_busca in remedio ["codigo"]. lower():
+            print("Remedio encontrado")
+            print(f"nome: {nome['nome']}")
+            print(f"codigo: {codigo ['codigo']}")
+            print(f"form_farmaceutica: {forma_farmaceutica ['form_farmaceutica']}")
+            print(f"preço: {preço ['preço']}")
+            
+            
+        
+    
+    
+    
 
 def menu_principal():
     progrma_rodando = True
